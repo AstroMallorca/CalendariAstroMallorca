04f9e6e489f0a4fb48ebfadb7a043cf1506e8d62
AstroMallorca <astromca@gmail.com>
2025-12-29 13:36:41 +0100

    Update app.js

--- app.js
+++ app.js
@@ -450,7 +450,7 @@ function dibuixaMes(isoYM) {
       ${act.length ? `<img class="am-bg" src="assets/icons/astromallorca.png" alt="" aria-hidden="true" loading="lazy">` : ""}
       <div class="num">${d}</div>
       <div class="badges">
-        ${esp.slice(0,6).map(x => `<img class="esp-icon" src="${x.codi}" alt="${(x.titol||x.clau||"").replace(/"/g,"&quot;")}" title="${(x.titol||"").replace(/"/g,"&quot;")}" loading="lazy">`).join("")}
+        ${esp.slice(0,6).map(x => `<img class="esp-icon" src="${x.icon}" alt="${(x.titol||"").replace(/"/g,"&quot;")}" loading="lazy">`).join("")}
       </div>
     `;
 
